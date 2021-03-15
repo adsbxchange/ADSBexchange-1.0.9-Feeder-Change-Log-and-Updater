45c165e75c1ab78b436fc43e0d275a992d66348d
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2021-03-15 11:49:34 -0700

    Update dhcpcd_fix.sh
    check for /etc/systemd/system/dhcpcd.service.d/restart.conf

--- dhcpcd_fix.sh
+++ dhcpcd_fix.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+FILE=/etc/systemd/system/dhcpcd.service.d/restart.conf
+if [[ -f "$FILE" ]]; then
+
+    echo "No updated needed."
+
+else
 
 sudo mkdir -p /etc/systemd/system/dhcpcd.service.d
 sudo tee /etc/systemd/system/dhcpcd.service.d/restart.conf <<"EOF"
@@ -11,3 +17,5 @@ StartLimitBurst=100
 EOF
 sudo systemctl daemon-reload
 sudo systemctl restart dhcpcd
+
+fi
